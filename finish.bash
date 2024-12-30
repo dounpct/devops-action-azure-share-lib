c96f42636afb423d4f2d10c868e46f5aaa099d6f
dounpct <doun_pct@hotmail.com>
2024-12-30 13:23:43 +0700

    test line noti

--- finish.bash
+++ finish.bash
@@ -2,5 +2,5 @@
 
 curl -X POST \
   -H "Authorization: Bearer ${LINE_TOKEN} " \
-  -F "message=Here is a notification with an StartUP Pipeline!" \
+  -F "message=Here is a notification with an Finish Pipeline!" \
   https://notify-api.line.me/api/notify
