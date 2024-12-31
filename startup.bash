cf3c14de178d048dfc294f5a09b80bdfef1e29a7
dounpct <doun_pct@hotmail.com>
2024-12-31 18:25:49 +0700

    test line noti

--- startup.bash
+++ startup.bash
@@ -4,12 +4,12 @@
 TOKEN=${LINE_TOKEN}
 
 # Message to Send (Supports Multi-Line)
-MESSAGE=$"${service_name}\nLine 2\nLine 3"
+MESSAGE="${service_name}\nLine 2\nLine 3"
 
 # Send the Notification
 curl -X POST \
   -H "Authorization: Bearer ${TOKEN}" \
-  -F "message=${MESSAGE}" \
+  -F $"message=${MESSAGE}" \
   https://notify-api.line.me/api/notify
 
 
