91c50036f0476e372e8673f4492340043f78482b
dounpct <doun_pct@hotmail.com>
2024-12-31 17:17:28 +0700

    test line noti

--- startup.bash
+++ startup.bash
@@ -1,18 +1,31 @@
 #!/bin/bash
 
-send_notification() {
-  local status=$1
-  local duration=$2
-  local log_url=$3
-  local msg_type=$4
+# LINE Notify Token
+TOKEN=${LINE_TOKEN}
 
-  message='{"text": "'"${status}"'GitHub Action: Action : '"${msg_type}"'\nType : Build with Tag\nApplication : '"${service_name}"'\nEnvironment : '"${environment}"'\nRef-Key : '"${image_tag}"'\nTagName : '"${tag_name}"'\nSHA1 : '"${short_sha}"'\nStatus : '"N/A"'\nDuration : '"${duration}"' seconds\nBy : '"${commit_user}"'\nLog : \"'"${log_url}"'\""}'
+# Message to Send (Supports Multi-Line)
+MESSAGE=$'Line 1\nLine 2\nLine 3'
+
+# Send the Notification
+curl -X POST \
+  -H "Authorization: Bearer ${TOKEN}" \
+  -F "message=${MESSAGE}" \
+  https://notify-api.line.me/api/notify
+
+
+# send_notification() {
+#   local status=$1
+#   local duration=$2
+#   local log_url=$3
+#   local msg_type=$4
+
+#   message='{"text": "'"${status}"'GitHub Action: Action : '"${msg_type}"'\nType : Build with Tag\nApplication : '"${service_name}"'\nEnvironment : '"${environment}"'\nRef-Key : '"${image_tag}"'\nTagName : '"${tag_name}"'\nSHA1 : '"${short_sha}"'\nStatus : '"N/A"'\nDuration : '"${duration}"' seconds\nBy : '"${commit_user}"'\nLog : \"'"${log_url}"'\""}'
   
-  curl -X POST \
-    -H "Authorization: Bearer ${LINE_TOKEN} " \
-    -F $"message=${message}!" \
-    https://notify-api.line.me/api/notify
-}
+#   curl -X POST \
+#     -H "Authorization: Bearer ${LINE_TOKEN} " \
+#     -F $"message=${message}!" \
+#     https://notify-api.line.me/api/notify
+# }
 
 
-send_notification "🚀" "N/A" "$GITHUB_SERVER_URL/$GITHUB_REPOSITORY/actions/runs/$GITHUB_RUN_ID" "START"
+# send_notification "🚀" "N/A" "$GITHUB_SERVER_URL/$GITHUB_REPOSITORY/actions/runs/$GITHUB_RUN_ID" "START"
