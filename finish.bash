0889809c067d8fe60ab6c34673545d2ce4367835
dounpct <doun_pct@hotmail.com>
2024-12-31 15:49:56 +0700

    test line noti

--- finish.bash
+++ finish.bash
@@ -1,6 +1,19 @@
 #!/bin/bash
 
-curl -X POST \
-  -H "Authorization: Bearer ${LINE_TOKEN} " \
-  -F "message=Here is a notification with an Finish Pipeline!" \
-  https://notify-api.line.me/api/notify
+send_notification() {
+  local status=$1
+  local duration=$2
+  local log_url=$3
+  local msg_type=$4
+
+  message='{"text": "'"${status}"'GitHub Action: Action : '"${msg_type}"'\nType : Build with Tag\nApplication : '"${service_name}"'\nEnvironment : '"${environment}"'\nRef-Key : '"${image_tag}"'\nTagName : '"${tag_name}"'\nSHA1 : '"${short_sha}"'\nStatus : '"N/A"'\nDuration : '"${duration}"' seconds\nBy : '"${commit_user}"'\nLog : \"'"${log_url}"'\""}'
+  
+  curl -X POST \
+    -H "Authorization: Bearer ${LINE_TOKEN} " \
+    -F "message=${message}!" \
+    https://notify-api.line.me/api/notify
+}
+
+
+send_notification "🚀" "N/A" "$GITHUB_SERVER_URL/$GITHUB_REPOSITORY/actions/runs/$GITHUB_RUN_ID" "FINISH"
+
